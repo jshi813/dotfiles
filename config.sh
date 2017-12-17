3f74359a6b604daf21f024d19110159d804cb3b8
Jason Shi <sjiachen@amazon.com>
2017-12-16 23:45:36 -0800

    Add vim setup script

--- config.sh
+++ config.sh
@@ -1,1 +1,5 @@
+# Set up vim
+mv ~/.vimrc ~/.vimrc_old
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c 'PluginInstall' -c 'qa!'
