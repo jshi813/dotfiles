71025966ab33cb07a396fb3424eda3093f4077cc
Jason Shi <sjiachen@amazon.com>
2017-12-16 23:50:49 -0800

    + zsh

--- config.sh
+++ config.sh
@@ -1,5 +1,11 @@
-# Set up vim
-mv ~/.vimrc ~/.vimrc_old
+# set up vim
+mv ~/.vimrc ~/.vimrc.orig
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c 'PluginInstall' -c 'qa!'
+
+# set up zsh
+#git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#mv ~/.zshrc ~/.zshrc.orig
+#cp .zshrc ~/.zshrc
+
